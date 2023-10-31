d89886bdca8d7bb7162c864b6dd4a2a85f6a1279
RafaelFirmo <93453022+RafaelFirmo@users.noreply.github.com>
2023-10-31 14:49:34 -0300

    Add files via upload

---
+++ CalculadoraPartidasRankeadas.js
@@ -0,0 +1,31 @@
+let resultado = calcularSaldo(11, 1);
+
+function calcularSaldo(vitorias, derrotas) {
+
+    let saldo = vitorias - derrotas;
+
+    if (saldo <= 10) {
+        nivel = "Ferro";
+} else if (saldo >= 11 && saldo <= 20) {
+        nivel = "Bronze";
+
+    } else if (saldo >= 21 && saldo <= 50) {
+        nivel = "Prata";
+
+    } else if (saldo >= 51 && saldo <= 80) {
+        nivel = "Ouro";
+
+    } else if (saldo >= 81 && saldo <= 90) {
+        nivel = "Diamante";
+
+    } else if (saldo >= 91 && saldo <= 100) {
+        nivel = "Lendário";
+
+    } else if (saldo >= 101) {
+        nivel = "Imortal";
+    }
+
+    return saldo
+};
+
+console.log(`O Herói tem de saldo de ${resultado} está no nível de ${nivel}`);
---
+++ ClassificadorNíveldeHeroi.js
@@ -0,0 +1,30 @@
+// variável para armazenar o nome do herói
+let nome = "Heitor";
+
+// variável para armazenar a quantidade de experiência do herói
+let xp = 5000;
+
+// variável para armazenar o nível do herói
+let nivel;
+
+// Estrutura de decisão para atribuir o nível do herói de acordo com a quantidade de experiência
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+  nivel = "Prata";
+} else if (xp >= 6001 && xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+  nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+// mensagem com o nome e o nível do herói
+console.log("O Herói de nome " + nome + " está no nível de " + nivel);
